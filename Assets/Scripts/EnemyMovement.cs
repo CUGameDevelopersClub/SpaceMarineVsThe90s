bd3d56517ba83e37009a06047f18d7fba54c4dcd
Joshua Khoo <joshua.khoo@colorado.edu>
2017-09-29 18:49:25 -0600

    Updated enemy movement script syntax

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -10,24 +10,23 @@ public class EnemyMovement : MonoBehaviour {
 
     bool isMovingLeft = false;
 
-    Random rand = new Random();
-    int direction = rand.Next(1, 3); // choose random direction
+    int direction = Random.Range(1, 3); // choose random direction
 	
 	void Update () {
-        direction = rand.Next(1, 3);
+        direction = Random.Range(1, 3);
 
         // If moving right and position is -100
         // Move in a random direction
         // If moving right, set moving left to false
         // If moving left, set moving left to true
-	    if ( !movingLeft && transform.localPosition.x <= -100  && direction == 2) {
-            moveDirection = newVector3(1, 0, 0);
+	    if ( !isMovingLeft && transform.localPosition.x <= -100  && direction == 2) {
+            moveDirection = new Vector3(1, 0, 0);
             isMovingLeft = (direction == 2 ) ? true : false;
         }
         // If moving left and position is 100
         // Move in a random direction
         else if ( isMovingLeft && transform.localPosition.x >= 100 && direction == 1) {
-            moveDirection = newVector3(-1, 0, 0);
+            moveDirection = new Vector3(-1, 0, 0);
             isMovingLeft = ( direction == 1 ) ? true : false;
         }
 
