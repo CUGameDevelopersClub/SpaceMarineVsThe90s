24887e761fdb0e2db540bf11f85c712da0cd608b
Jarrod Raine <jarrodfraine@me.com>
2017-09-30 21:26:23 -0600

    Simplied Camera script

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,23 +4,18 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-    public Transform player;
-    public Vector3 offset; // offset distance between player and camera
+	private GameObject player;
+    private Vector3 offset; // offset distance between player and camera
 
 	// Use this for initialization
 	void Start () {
-        offset = transform.position - player.transform.position;
+		offset = this.GetComponent<Transform>().position;
+		player = GameObject.FindGameObjectWithTag("Player");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z);
-    }
 
     private void LateUpdate()
     {
         // prevents camera from following player rotation
-        float newXPos = player.transform.position.x - offset.x;
-        transform.position = new Vector3(newXPos, transform.position.y, transform.position.z);
+		transform.position = player.transform.position + offset;
     }
 }
