ae33860575f44faadc3635e2110b443bc55a1aa2
destroyer4645 <ian@usmeadows.com>
2017-10-04 22:55:23 -0600

    Added enemy Script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * 
+ * All Enemies should inherit from this
+ * 
+ * Must call setUp() on start up
+ * 
+ * */
+
+
+public class Enemy : MonoBehaviour {
+
+    public int health;
+
+    //Walking: collides with walls
+    //Flying: does not collide with walls
+    //might add more
+    public enum EnemyType { Walking, Flying};
+    public EnemyType enemyType;
+
+    //basic enemy setup
+    public void setUp(int health, EnemyType enemytype)
+    {
+        this.health = health;
+        this.enemyType = enemytype;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //checks the health. If lower then 0 destroy
+    public void checkHealth()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
