da1de81031a6c9f95c373f81e43a30549771ca18
MajorStorm <jddevstudio@gmail.com>
2017-10-06 19:14:14 -0600

    PUUUUUSH #2
    Project clean up.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,42 +5,20 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
     
-    public static GameManager instance = null;
-    private int difficultyLevel;
     private int playerHealth;
-
     private int level = 1;
-    private bool isEnemyMoving;
-    private bool doingSetup;
 
-    public Transform spawnPoint;
-
-    private void Awake()
-    {
-
-    }
-
-
-    // Use this for initialization
     void Start ()
     {
 
 	}
 	
-	// Update is called once per frame
 	void Update ()
     {
         
 	}
 
-    void initGame()
-    {
+    private void GenerateLevel() {
 
     }
-
-    protected virtual void genWorld() { }
-
-    protected virtual void spawnWorld() { }
-
-    protected virtual void checkLife() { }
 }
