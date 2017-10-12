fd73992c5dcfadd787bca61c48cd10bf9a84eb3f
Joshua Khoo <joshua.khoo@colorado.edu>
2017-10-12 01:00:53 -0600

    player movement now uses rb2d, added a jump script, fixed climbing bugs

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     private static Player instance;
+    private Rigidbody2D rb2d;
 
     // hard-coded in right now, can be changed later
-    public float moveSpeed = 5.5f;
-    public float climbSpeed = 6f;
+    public float moveSpeed;
+    public float climbSpeed;
+    public float jumpForce;
 
     public bool onRope;
 
@@ -25,11 +27,16 @@ public class Player : MonoBehaviour {
     private void Start() {
         onRope = false;
         Physics2D.gravity = new Vector2(0, -10f);
+        rb2d = GetComponent<Rigidbody2D>();
+
+        moveSpeed = 5.5f;
+        climbSpeed = 6f;
+        jumpForce = 200f;
     }
 
-    void Update() {
-        Movement();
-        if (Input.GetKey(KeyCode.E))
+    void FixedUpdate() {
+        Movement(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if (Input.GetKeyDown(KeyCode.E))
             Use();
     }
 
@@ -38,39 +45,32 @@ public class Player : MonoBehaviour {
             useable.Use();
     }
 
-    void Movement() {
-        // left movement
-        if (Input.GetKey(KeyCode.A)) 
-            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
-
-        // right movement
-        if (Input.GetKey(KeyCode.D))
-            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+    void Movement(float horizontal, float vertical) {
+        rb2d.velocity = new Vector2(horizontal * moveSpeed, rb2d.velocity.y);
 
         // climbing
-        if (onRope && Input.GetKey(KeyCode.W)) {
-            transform.position += Vector3.up * climbSpeed * Time.deltaTime;
-            Physics2D.gravity = Vector2.zero;
-        } else if ( onRope && Input.GetKey(KeyCode.S)) {
-            transform.position += Vector3.down * climbSpeed * Time.deltaTime;
-            Physics2D.gravity = Vector2.zero;
-        } else if (Input.GetKey(KeyCode.Space)) {
+        if (onRope)
+            rb2d.velocity = new Vector2(horizontal * climbSpeed, vertical * climbSpeed);
+        if (onRope && Input.GetKey(KeyCode.Space)) {
             onRope = false;
         }
 
-        if (!onRope)
-            Physics2D.gravity = new Vector2(0, -10f);
+        // jump
+        if (rb2d.velocity.y == 0 && Input.GetButton("Jump") && !onRope)
+            rb2d.AddForce(new Vector2(0, jumpForce));
     }
 
     private void OnTriggerStay2D(Collider2D other) {
         if (other.tag == "Rope") {
             onRope = true;
+            Physics2D.gravity = Vector2.zero;
         }
     }
 
     private void OnTriggerExit2D(Collider2D other) {
         if (other.tag == "Rope") {
             onRope = false;
+            Physics2D.gravity = new Vector2(0, -10f);
         }
     }
 }
